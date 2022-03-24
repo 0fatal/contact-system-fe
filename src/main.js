365fe9a1a738b4f6501f4665f692c80f8ae39ce5
ZacharyWin <2816813070@qq.com>
2022-03-24 20:34:25 +0800

    chore

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import store from './store'
+import './main.css'
 
 Vue.use(ElementUI)
 Vue.config.productionTip = false
@@ -10,4 +12,5 @@ Vue.config.productionTip = false
 new Vue({
     router,
     render: (h) => h(App),
+    store,
 }).$mount('#app')
